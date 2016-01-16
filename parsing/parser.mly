def-lkb/ocaml

WIP introducing implicit in typing

parsing/parser.mly
@@ -651,8 +651,8 @@ structure_item:
   | MODULE TYPE ident EQUAL module_type post_item_attributes
       { mkstr(Pstr_modtype (Mtd.mk (mkrhs $3 3)
                               ~typ:$5 ~attrs:$6 ~loc:(symbol_rloc()))) }
-  | IMPLICIT MODULE module_binding
-      { mkstr(Pstr_implicit $3) }
+  | IMPLICIT implicit_binding
+      { mkstr(Pstr_implicit $2) }
   | OPEN override_flag mod_longident post_item_attributes
       { mkstr(Pstr_open ($2, mkrhs $3 3, $4)) }
   | CLASS class_declarations
@@ -679,6 +679,32 @@ module_bindings:
 module_binding:
     UIDENT module_binding_body post_item_attributes
     { Mb.mk (mkrhs $1 1) $2 ~attrs:$3 ~loc:(symbol_rloc ()) }
+;
+
+
+implicit_parameter:
+  | LPAREN functor_arg_name COLON module_type RPAREN
+      { Ip.mk (mkrhs $2 2) $4 ~loc:(symbol_rloc ()) }
+;
+implicit_parameters:
+    implicit_parameters implicit_parameter
+      { $2 :: $1 }
+  | implicit_parameter
+      { [ $1 ] }
+;
+
+implicit_binding:
+    MODULE UIDENT implicit_binding_body
+    { Ib.mk (mkrhs $2 2) [] $3 ~loc:(symbol_rloc ()) }
+  | FUNCTOR UIDENT implicit_parameters implicit_binding_body
+    { Ib.mk (mkrhs $2 2) $3 $4 ~loc:(symbol_rloc ()) }
+;
+
+implicit_binding_body:
+    EQUAL module_expr
+    { $2 }
+  | COLON module_type EQUAL module_expr
+    { mkmod(Pmod_constraint($4, $2)) }
 ;
 
 /* Module types */

Kakadu/ocaml-modular-implicits

parsing/parser.mly
@@ -767,8 +767,8 @@ structure_item:
   | module_type_declaration
       { mkstr(Pstr_modtype $1) }
   | open_statement { mkstr(Pstr_open $1) }
-  | IMPLICIT MODULE module_binding
-      { mkstr(Pstr_implicit $3) }
+  | IMPLICIT module_binding
+      { mkstr(Pstr_implicit $2) }
   | class_declarations
       { mkstr(Pstr_class (List.rev $1)) }
   | class_type_declarations
@@ -812,6 +812,32 @@ and_module_binding:
     AND UIDENT module_binding_body post_item_attributes
       { Mb.mk (mkrhs $2 2) $3 ~attrs:$4 ~loc:(symbol_rloc ())
                ~text:(symbol_text ()) ~docs:(symbol_docs ()) }
+;
+
+
+implicit_parameter:
+  | LPAREN functor_arg_name COLON module_type RPAREN
+      { Ip.mk (mkrhs $2 2) $4 ~loc:(symbol_rloc ()) }
+;
+implicit_parameters:
+    implicit_parameters implicit_parameter
+      { $2 :: $1 }
+  | implicit_parameter
+      { [ $1 ] }
+;
+
+implicit_binding:
+    MODULE UIDENT implicit_binding_body
+    { Ib.mk (mkrhs $2 2) [] $3 ~loc:(symbol_rloc ()) }
+  | FUNCTOR UIDENT implicit_parameters implicit_binding_body
+    { Ib.mk (mkrhs $2 2) $3 $4 ~loc:(symbol_rloc ()) }
+;
+
+implicit_binding_body:
+    EQUAL module_expr
+    { $2 }
+  | COLON module_type EQUAL module_expr
+    { mkmod(Pmod_constraint($4, $2)) }
 ;
 
 /* Module types */